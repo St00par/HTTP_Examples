9d467cdef14ccb01ae4884ccae5bd823f3a47619
Your Name <you@example.com>
2021-08-03 09:23:52 +0100

    Iteration and Contitionals commited and tidied up previous work.

---
+++ Conditionals/JavaScript2.js
@@ -0,0 +1,28 @@
+// conditionals 1
+// let strictA = true;
+// let strictB = 1;
+// true:
+// console.log(strictA == strictB); 
+// false:
+// console.log(strictA === strictB);
+
+// conditionals 2
+// false:
+// console.log(strictA != strictB);
+// true:
+// console.log(strictA !== strictB);
+
+// conditionals 3
+// let age = 74;
+// if (age >=18 && age <=65) {
+//     console.log("You get could get unemployment benefit")
+//       } else if (age <18) {
+//         console.log("You get child benefit");
+//       } else {
+//         console.log("You get a state pension");
+//       }
+
+// conditionals 4
+// let age = 55;
+// let eligibility = "Are you are eligible for an over 50s life plan? " + (age > 50 ? "YES! Apply Now." : "No, you are too young.");
+// console.log(eligibility);
---
+++ Iterations/JavaScript1.js
@@ -0,0 +1,54 @@
+// iteration 1 & 4
+// for (let a = 100; a < 200; a++) {
+// console.log(`a = ${a}`);
+// }
+
+// // iteration 2 & 4
+// for (let a = 100; a <= 200; a++) {
+// 			if (a % 2 == 0) {
+// 				console.log(`- ${a}`);
+// 			} else {
+// 				console.log(`* ${a}`);
+// 			}
+// 		}
+
+// iteration 3
+// for (let i = 1; i <= 10; i++) {
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(j);
+//     }
+// }
+
+// and each number the same amount of times as it's value
+// for (let i = 1; i <= 10; i++) {
+//     for (let j = 0; j < i; j++) {
+//         console.log(i);
+//     }
+// }
+
+// iteration 5
+// let now = new Date(); 
+// let day = now.getDay();
+
+// switch (day) {
+// case 0:
+//     console.log("Sunday");
+//     break;
+// case 1:
+//     console.log("Monday");
+// case 2:
+//     console.log("Tuesday");
+// case 3:
+//     console.log("Wednesday");
+// case 4:
+//     console.log("Thursday");
+// case 5:
+//     console.log("Friday");
+//     break;
+// case 6:
+//     console.log("Saturday");
+//     break;
+// default:
+//     console.log("You have not selected a day of the week!");
+//     break;
+// }
